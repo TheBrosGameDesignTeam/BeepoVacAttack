d9b2c9ecfb104697509bfd9d3ba1bd4e0739f045
Laurel Anderson <laurel.o.anderson@wsu.edu>
2021-11-09 08:45:16 -0800

    Add basic start screen

---
+++ BeepoVacAttack/src/BeepoVacFiles/MainGame.java
@@ -0,0 +1,41 @@
+package BeepoVacFiles;
+
+import jig.Entity;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainGame extends StateBasedGame {
+
+    public static final int STARTUPSTATE = 0;
+
+    public MainGame(String title, int width, int height) {
+        super(title);
+        Entity.setCoarseGrainedCollisionBoundary(Entity.AABB);
+
+    }
+
+
+    @Override
+    public void initStatesList(GameContainer container) throws SlickException {
+        addState(new StartUpState());
+    }
+
+    public static void main(String[] args) {
+        AppGameContainer app;
+        try {
+            app = new AppGameContainer(new MainGame("BeepoVacAttack!", 800, 600));
+            app.setDisplayMode(800, 600, false);
+            app.setVSync(true);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
+
