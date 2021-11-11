0ec38b850060a8270e41ee5ce9ab08bc5f5bf343
JohnStevensonWSU <78937852+JohnStevensonWSU@users.noreply.github.com>
2021-11-11 11:29:15 -0800

    Add files via upload

---
+++ BeepoVacAttack/src/BeepoVacFiles/Caller.java
@@ -0,0 +1,35 @@
+package BeepoVacClient;
+
+import java.io.PrintWriter;
+
+public class Caller extends Thread {
+
+    private final PrintWriter printWriter;
+
+    public Caller(PrintWriter printWriter) {
+        this.printWriter = printWriter;
+    }
+
+    public void run() {
+        printWriter.println("Hello, Server!");
+        printWriter.flush();
+
+//        while (true) {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(i);
+                printWriter.println(i);
+                printWriter.flush();
+                try {
+                    sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+//            try {
+//                sleep(1000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+    }
+}
