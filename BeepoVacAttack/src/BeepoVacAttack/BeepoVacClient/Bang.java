fb103ef0db78999547efd5016aa5919f40205a35
Irina Bejan <irina.bejan@wsu.edu>
2021-12-16 00:44:52 -0800

    Add poof when BeepoVac catches bunny

--- BeepoVacAttack/src/BeepoVacAttack/BeepoVacClient/Bang.java
+++ BeepoVacAttack/src/BeepoVacAttack/BeepoVacClient/Bang.java
@@ -16,7 +16,7 @@ public class Bang extends Entity {
         explosion = new Animation(sprite, 50);
         addAnimation(explosion);
         explosion.setLooping(false);
-
+        ResourceManager.getSound(MainGame.POOF_SOUND).play();
     }
 
     public boolean isActive() {
